ca0ca180b517453f414a6e32db9975d7341571d4
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-12-01 18:58:01 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nephyy | Portfolio",
+  description: "Personal Portfolio of Nephyy",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id" className="scroll-smooth">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
